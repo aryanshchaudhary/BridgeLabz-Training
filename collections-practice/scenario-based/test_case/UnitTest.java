39a8b057619d1a73da3f5a4a458f5c0c547f450a
Aryansh Chaudhary <abhichaudhary012002@gmail.com>
2026-02-09 21:29:52 +0530

    [Aryansh Chaudhary] add . test_case : problem solved

---
+++ collections-practice/scenario-based/test_case/UnitTest.java
@@ -0,0 +1,45 @@
+package com.example.junit_practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+
+    @Test
+    public void Test_Deposit_ValidAmount() {
+        Program account = new Program(100);
+        account.deposit(50);
+
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    public void Test_Deposit_NegativeAmount() {
+        Program account = new Program(100);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            account.deposit(-20);
+        });
+
+        assertEquals("Deposit amount cannot be negative", exception.getMessage());
+    }
+
+    @Test
+    public void Test_Withdraw_ValidAmount() {
+        Program account = new Program(200);
+        account.withdraw(50);
+
+        assertEquals(150, account.getBalance());
+    }
+
+    @Test
+    public void Test_Withdraw_InsufficientFunds() {
+        Program account = new Program(100);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            account.withdraw(200);
+        });
+
+        assertEquals("Insufficient funds.", exception.getMessage());
+    }
+}
